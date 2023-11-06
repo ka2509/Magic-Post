70be3875edcde53c4732e39b16c956aaf057bbb7
2uangAnh <doquanganhzzz2k3@gmail.com>
2023-11-06 22:07:13 +0700

    init backend project

---
+++ Backend/MagicPost/src/test/java/com/nhom10/MagicPost/MagicPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom10.MagicPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
