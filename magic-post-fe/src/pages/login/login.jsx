bf3f77624961e205e0786a1ccc3e67ff49647dd6
QDuongDubb <phamthiquyduong@gmail.com>
2023-12-24 13:47:23 +0700

    feat/qduong/magicpost-fe

--- magic-post-fe/src/pages/login/login.jsx
+++ magic-post-fe/src/pages/login/login.jsx
@@ -1,48 +1,64 @@
 import React, { useState } from "react";
 import AuthenticationServices from "../../services/AuthenticationServices";
+import Navbar from "../../components/navbar/navbar";
+import LoginBachGround from "../../assets/47f24d49376753.58b346965a50a.png"
+import "./login.css";
 
-function Login(){
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState("");
-    const [loginFailed,setLoginFailed] = useState(false)
-    const handleSubmit = async (event) =>{
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginFailed, setLoginFailed] = useState(false)
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        try{
+        try {
             const user = {
                 username: username,
                 password: password
             }
             const response = await AuthenticationServices.login(user)
             console.log(response)
-            localStorage.setItem("token",response.data.token)
-            window.location.href="/dashboard"
-        } catch (error){
+            localStorage.setItem("token", response.data.token)
+            window.location.href = "/dashboard"
+        } catch (error) {
             console.log("login failed")
             setLoginFailed(true)
         }
     }
-    return(
-        <div className="container">
-            <div className="login-form" onSubmit={handleSubmit}>
-                <form>
-                    <h1>User Login</h1>
-                    <input
-                        type="text"
-                        id="username"
-                        required
-                        onChange={(event) => setUsername(event.target.value)}
-                    >
-                    </input>
-                    <input
-                        type="password"
-                        id="password"
-                        required   
-                        onChange={(event) => setPassword(event.target.value)} 
-                    >
-                    </input>
-                    <button className="button" type="submit" value="login">Login</button>
-                    {loginFailed ? <span>login failed</span>: <span>&nbsp;</span>}
-                </form>
+    return (
+        <div className="login">
+            <Navbar />
+            <div className="container">
+                <img src={LoginBachGround} alt="loginBG"></img>
+                <div className="login-form" onSubmit={handleSubmit}>
+                    <form>
+                        <h1>User Login</h1>
+                        <div>
+                            <div className="userInput">
+                                <input
+                                    type="text"
+                                    id="username"
+                                    required
+                                    onChange={(event) => setUsername(event.target.value)}
+                                >
+                                </input>
+                            </div>
+                            <div className="userInput">
+                                <input
+                                    type="password"
+                                    id="password"
+                                    required
+                                    onChange={(event) => setPassword(event.target.value)}
+                                >
+                                </input>
+                            </div>
+                            <div>
+                                {loginFailed ? <span className="loginStat">login failed</span> : <span>&nbsp;</span>}
+
+                            </div>
+                            <button className="button" type="submit" value="login">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
