4c221a9ea7f31a11d319d69cde24bd9ee8f487e1
2uangAnh <doquanganhzzz2k3@gmail.com>
2023-12-29 00:34:12 +0700

    update component

--- magic-post-fe/src/components/userInfo/UserPortrait.jsx
+++ magic-post-fe/src/components/userInfo/UserPortrait.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { LogOut, LogOutOutline, PersonOutline } from 'react-ionicons';
+import UserServices from '../../services/UserServices';
 
 function UserPortrait() {
+    const [user, setUser] = useState({});
+    useEffect(() => {
+        const getUser = async () => {
+            const response = await UserServices.getCurrentUser();
+            setUser(response.data);
+        }
+        getUser();
+    }, []);
+    const LogOut = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
+        window.location.href = '/';
+    }
+
     return (
         <div className='userportrait'>
+            
             <div className='user'>
-                <img alt='avt'></img>
-                user name here
+            <PersonOutline
+                    color={'#00000'}
+                    height="25px"
+                    width="25px"
+                />
+                {user.username}
             </div>
-            <a href='$'>log out</a>
+            <a href='$'>
+                <LogOutOutline
+                    color={'#00000'}
+                    height="25px"
+                    width="25px"
+                    onClick={() => LogOut()}
+                /></a>
         </div>
     );
 }
