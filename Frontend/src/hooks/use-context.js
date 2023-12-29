daf523bba8e70b4e8b742391cec587049c05c9ff
2uangAnh <doquanganhzzz2k3@gmail.com>
2023-12-29 16:21:42 +0700

    modify code

--- Frontend/src/hooks/use-context.js
+++
@@ -1,8 +0,0 @@
-import { AppContext } from "@components/context/AppContext";
-import { useContext } from "react";
-
-const useGlobalContext = () => {
-  return useContext(AppContext);
-};
-
-export default useGlobalContext;
